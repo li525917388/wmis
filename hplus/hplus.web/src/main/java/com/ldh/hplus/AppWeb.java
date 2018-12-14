bf490b7cdb67db564e44ad8cdd4447c60b7e68e7
ldh <525917388@qq.com>
2018-12-14 17:51:38 +0800

    eeee

--- hplus/hplus.web/src/main/java/com/ldh/hplus/AppWeb.java
+++ hplus/hplus.web/src/main/java/com/ldh/hplus/AppWeb.java
@@ -3,14 +3,28 @@ package com.ldh.hplus;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * 
+ * @author Li Dehuan
+ * @date 2018年12月14日
+ *
+ */
 @MapperScan(basePackages={"com.ldh.hplus.*.dao"})
 @SpringBootApplication
-public class AppWeb {
+public class AppWeb extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		
 		SpringApplication.run(AppWeb.class, args);
 	}
 
+	@Override//为了打包springboot项目 
+	protected SpringApplicationBuilder configure( 
+		SpringApplicationBuilder builder) { 
+		return builder.sources(this.getClass()); 
+	} 
+	
 }
